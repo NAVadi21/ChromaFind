79ce8c232f3def560a72526c54fe10719a8a807c
PRANAV ADITYA SINGH <93078167+NAVadi21@users.noreply.github.com>
2023-02-18 16:49:19 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,9 @@
+these are the packages you will need to run this code in PyCharm:
+
+OpenCV: Used for capturing and processing video frames. You can install it by running pip install opencv-python in the terminal or the PyCharm package manager.
+
+
+NumPy: Used for handling arrays and numerical calculations. You can install it by running pip install numpy in the terminal or the PyCharm package manager.
+
+
+These are the only two packages you need to run this code in PyCharm. You may also need to install other packages depending on the requirements of your project.
---
+++ main.py
@@ -0,0 +1,64 @@
+import cv2
+import numpy as np
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_BUFFERSIZE, 1)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+cap.set(cv2.CAP_PROP_FPS, 60)
+
+
+while True:
+    # Read a video frame
+    ret, frame = cap.read()
+
+    # Convert the frame to the HSV color space
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    # Define ranges for different colors
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    lower_green = np.array([40, 50, 50])
+    upper_green = np.array([80, 255, 255])
+    lower_blue = np.array([100, 50, 50])
+    upper_blue = np.array([140, 255, 255])
+    lower_yellow = np.array([20, 50, 50])
+    upper_yellow = np.array([30, 255, 255])
+
+    # Threshold the image to get only the desired colors
+    red_mask = cv2.inRange(hsv, lower_red, upper_red)
+    green_mask = cv2.inRange(hsv, lower_green, upper_green)
+    blue_mask = cv2.inRange(hsv, lower_blue, upper_blue)
+    yellow_mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+
+    # Find contours for the colored objects
+    red_contours, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    green_contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    blue_contours, _ = cv2.findContours(blue_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    yellow_contours, _ = cv2.findContours(yellow_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Draw squares around the contours
+    for cnt in red_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+    for cnt in green_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    for cnt in blue_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+    for cnt in yellow_contours:
+        x, y, w, h = cv2.boundingRect(cnt)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 255), 2)
+
+    # Display the resulting frame
+    cv2.namedWindow("OUTPUT", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("OUTPUT", 1000, 600)
+    cv2.imshow('OUTPUT', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam
+cap.release()
+cv2.destroyAllWindows()
